3491f1bbaf03cad263f4b7f09176c0dc49e86cd5
ekizsemih <semihekiz8@gmail.com>
2015-11-30 10:03:24 +0200

    Initial Commit

---
+++ iStalk/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iStalk
+//
+//  Created by Semih EKIZ on 30/11/15.
+//  Copyright © 2015 Semih EKIZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iStalk/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iStalk
+//
+//  Created by Semih EKIZ on 30/11/15.
+//  Copyright © 2015 Semih EKIZ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
